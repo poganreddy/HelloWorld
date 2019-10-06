2de5601ce2824c34e2dc9d5e744768990a164f7e
pogan <poganhadoop@gmail.com>
2019-10-06 20:01:47 +0530

    updated java class name

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,4 +1,4 @@
-public class HelloWorldForTesting {
+public class HelloWorld {
    public static void main(String[] args) {
       // Prints "Hello, World" in the terminal window.
 	  for(int i=0;i<10;i++){
